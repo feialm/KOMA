935504b4b2ff8e039f671f860c5e96ebab37a440
jesschowh <57443463+jesschowh@users.noreply.github.com>
2019-12-02 15:21:15 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -15,11 +15,11 @@ function Parent(){
     window.localStorage.setItem("data", JSON.stringify(a));
     setRedirect(true);
   }
-//denna eller funktion gör vi i falla att det är null i localstarage
+
+  //denna eller funktion gör vi i falla att det är null i localstarage
   function loadData(){
     try {
       const storage = JSON.parse(window.localStorage.getItem("data"));
-      console.log(storage);
       return storage || [];
     } catch (e) {
       return [];
@@ -32,148 +32,106 @@ function Parent(){
   }
 
   function fixObject (a, b, c, d, e) {
-console.log("Ifix")
-console.log(a)
-console.log(b)
-console.log(c)
-console.log(d)
 
-console.log(e)
+    let TotTime = (Number(d)*60) + Number(e);
+    let g = String(0); //reptime
+    let f = String(TotTime);
 
-let name = a;
-let startDate = b;
-let endDate = c;
-let hours = d;
-let min = e;
+    const Course = {name: a, startDate: b, endDate: c, totTime: f, repTime: g };
+     
+    addtoList(Course);
+  }
 
-let TotTime = (Number(d)*60) + Number(e);
-let g = String(10); //reptime
-let f = String(TotTime);
-console.log(TotTime)
+  function addtoList(courseToAdd){
+    localData.push(courseToAdd);
+    setData(localData);
+  }
 
-
-const Course = {name: a, startDate: b, endDate: c, totTime: f, repTime: g };
-console.log(Course);
- 
-addtoList(Course);
-//getCourseName(Course);
-
-}
-
-
-function addtoList(courseToAdd){
-
-  //count = count + 1;
-  localData.push(courseToAdd);
- // console.log("Here´s the array");
- // console.log(courseArray);
-  //console.log(courseArray[0]);
- // const array = [];
-  //{array.map(heading, index) => <div key={index}>{heading}</div>)};
-
-
-  setData(localData);
-
-}
-
-
-function routerApp(){
-return(
-
-
-<Router>
-<div>
-<li>
-  <Link to="/Child">Child</Link>
-</li>
-</div>
-
-
-
-<Switch>
-<Route path="/Child" component={Child} />
-
-</Switch>
-</Router>);
-
-}
+  function routerApp(){
+    return(
+      <Router>
+        <div>
+          <li>
+            <Link to="/Child">Child</Link>
+          </li>
+        </div>
+        <Switch>
+          <Route path="/Child" component={Child} />
+        </Switch>
+      </Router>
+    );
+  }
 
   return(
-
-
-  <div>
-
-  {redirect === false && 
-  <App fixObjectEtikett={fixObject} />
-  }
-  
-  {redirect === true && 
-  <Child courses={localData} />
-  }
-  
-  
-
-  </div>
-
+    <div>
+      {redirect === false && 
+      <App fixObjectEtikett={fixObject} />
+      }
+      {redirect === true && 
+      <Child courses={localData} />
+      }
+    </div>
   );
-
-
-
-
-
 }
 
 
 //App är form
 function App(props) { // props eller inte props??
 
-const [name, setCoursName] = useState({name: ''});
-const [startDate, setStartDate] = useState({startDate: 0});
-const [endDate, setEndDate] = useState({endDate: 0});
-const [hours, setHours] = useState({setH: 0});
-const [min, setMin] = useState({setMin: 0});
+  const [name, setCoursName] = useState({name: ''});
+  const [startDate, setStartDate] = useState({startDate: 0});
+  const [endDate, setEndDate] = useState({endDate: 0});
+  const [hours, setHours] = useState({setH: 0});
+  const [min, setMin] = useState({setMin: 0});
 
 
-function changeInput(event){
-  //console.log(event.target.id);
-if(event.target.id === "name"){
-    setCoursName(event.target.value);
-  }
-else if(event.target.id === "startDate"){
-  setStartDate(event.target.value);
-}
-else if(event.target.id === "endDate"){
-setEndDate(event.target.value);
-}
-else if(event.target.id === "hours"){
-  setHours(event.target.value);
-}
-else{
-    setMin(event.target.value);
-}
-    
-
+  function changeInput(event){
+    if(event.target.id === "name"){
+        setCoursName(event.target.value);
+      }
+    else if(event.target.id === "startDate"){
+      setStartDate(event.target.value);
+    }
+    else if(event.target.id === "endDate"){
+    setEndDate(event.target.value);
+    }
+    else if(event.target.id === "hours"){
+      setHours(event.target.value);
+    }
+    else{
+        setMin(event.target.value);
+    }
   } 
 
-
   return (
-
     <div className="App">
-
-      /////////////consloe.log("Hej");
-
-      <h1> Form </h1>
-      <input id="name" type="text" placeholder="Course..." onChange={changeInput} />
-      
-      <div> <input id="startDate" type="text" placeholder="StartDate..." onChange={changeInput} /> </div>
-      <div> <input id="endDate" type="text" placeholder="EndDate..." onChange={changeInput} /> </div>
-
-      <div> <input id="hours" type="text" placeholder="Hours..." onChange={changeInput} /> </div>
-      <div> <input id="min" type="text" placeholder="Min..." onChange={changeInput} /> </div>
-      
-      <button onClick={() => props.fixObjectEtikett(name,startDate,endDate,hours,min)} className="button"> Save </button>
-      
-
+      <div className="App-header">
+        <h1>Add Activity</h1>
+      </div>
+      <div className="MainBody">
+        <div className="WrapperAddActivity">
+          <div className="WrapperNameDates">
+            <p>Activity Name</p>
+            <input id="name" type="text" placeholder="Course name..." onChange={changeInput}/>
+          </div>
+          <div className="WrapperNameDates"> 
+            <p>Start Date</p> 
+            <input id="startDate" type="text" placeholder="Start Date..." onChange={changeInput}/>
+          </div>
+          <div className="WrapperNameDates">
+            <p>End Date</p>
+            <input id="endDate" type="text" placeholder="EndDate..." onChange={changeInput}/>
+          </div>
+          <div className="WrapperAddTime">
+          <p>Set Time</p>
+            <input id="hours" type="text" placeholder="Hours..." onChange={changeInput}/>
+            <p>h</p>
+            <input id="min" type="text" placeholder="Min..." onChange={changeInput}/>
+            <p>m</p>
+          </div>
+          <button onClick={() => props.fixObjectEtikett(name,startDate,endDate,hours,min)} className="button">Save</button>
+        </div>
+      </div>
     </div>
   );
 }
